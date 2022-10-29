ba88e07c1e26ed422bd0aa5e3f53732a7b7ef1cc
Vladislav Kochegin <kocheginvladislav@gmail.com>
2022-10-29 21:41:27 +0200

    Add entry point for game

--- Durak/Durak.ConsoleUI/Program.cs
+++ Durak/Durak.ConsoleUI/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace Durak.ConsoleUI
+﻿using Durak.Logic;
+using Durak.Logic.Models;
+
+namespace Durak.ConsoleUI
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var deckAndTrump = new DeckGenerator().Generate();
+            var deck = deckAndTrump.deck;
+            var trump = deckAndTrump.trump;
+            var playTable = new PlayTable(trump.CardSuit);
+            var human = new Human(playTable, deck);
+            var bot = new Bot(playTable, deck);
+            var consoleGraphics = new ConsoleGraphics(human, bot, playTable, deck, trump);
+
+            new Game(deckAndTrump.deck, playTable, human, bot, consoleGraphics).Play();
         }
     }
 }
