7bf956434968ec816db20290a36f464ed55fa34e
Vladislav Kochegin <kocheginvladislav@gmail.com>
2022-10-29 21:39:24 +0200

    Add human

---
+++ Durak/Durak.ConsoleUI/Human.cs
@@ -0,0 +1,41 @@
+﻿using Durak.Logic;
+using Durak.Logic.AbstractClasses;
+using Durak.Logic.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Durak.ConsoleUI
+{
+    internal class Human : Player
+    {
+        public Human(PlayTable playTable, Stack<Card> deck) : base(playTable, deck)
+        {
+        }
+
+        public override bool Play()
+        {
+            string input;
+            int index;
+            while (true)
+            {
+                input = Console.ReadLine();
+                if (int.TryParse(input, out index) && index >= 1 && index <= _cards.Count && _playTable.AddCard(_cards[index - 1]))
+                {
+                    _cards.RemoveAt(index - 1);
+                    return true;
+                }
+                else if (input == "n")
+                {
+                    return false;
+                }
+                else
+                {
+                    --Console.CursorTop;
+                }
+            }
+        }
+    }
+}
